9c47908a1dab0b05595f7643eec3e0fd565337b6
Mannomatico <maximilianzaun@gmail.com>
2022-12-20 01:23:05 +0100

    Main Method Testing

--- BlackBoxGraph/src/Main.java
+++ BlackBoxGraph/src/Main.java
@@ -1,4 +1,14 @@
+import  java.util.List;
+import  interfaces.IGraph;
+import  loader.PluginLoader;
 
 public class Main {
+	
+	public static void main(String[] args) {
+		List<IGraph> graphList = PluginLoader.load(IGraph.class);
+		for(IGraph graph : graphList) {
+			graph.getNumVertices();
+		}
+	}
 	
 }
