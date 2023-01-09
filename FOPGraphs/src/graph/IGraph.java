a732f48e03165137fff5f4fa2856758e50052b49
Tilman Hartwig <tilman.hartwig@gmail.com>
2023-01-09 18:21:59 +0100

    moved files and turned Graph into non abstract class

---
+++ FOPGraphs/src/graph/IGraph.java
@@ -0,0 +1,17 @@
+package graph;
+
+import java.util.HashMap;
+import java.util.List;
+
+import edge.Edge;
+import node.Node;
+
+public interface IGraph {
+	public void addEdge(Edge edge);
+	public HashMap<Node, List<Edge>> getEdgesByVertices();
+	public List<Edge> getEdgesAsList();
+	public int getNumVertices();
+	public void Traverse(Node start);
+	public IGraph MST();
+	public void getAdjacencyMatrix();
+}
